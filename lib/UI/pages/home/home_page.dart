379b3834fec4978ad09fbf64f7227b2cc4b68070
Cristian Cubillos <ccubillos@redsis.com>
2024-09-17 14:00:44 -0500

    Add CalendarTimeline to HomePage and update dependencies

--- lib/UI/pages/home/home_page.dart
+++ lib/UI/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_timeline/calendar_timeline.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,24 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: CalendarTimeline(
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2024, 1, 15),
+        lastDate: DateTime(2024, 12, 20),
+        onDateSelected: (date) => print(date),
+        leftMargin: 20,
+        monthColor: Colors.blueGrey,
+        dayColor: Colors.teal[200],
+        activeDayColor: Colors.white,
+        activeBackgroundDayColor: Colors.redAccent[100],
+        selectableDayPredicate: (date) => date.day >= DateTime.now().day,
+        locale: 'en_ISO',
+        height: MediaQuery.of(context).size.height * 0.3,
+      ),
+    );
   }
 }
