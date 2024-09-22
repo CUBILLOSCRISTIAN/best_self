ce7ed237cf22f51968917ad1a14495f433ed0c2c
Cristian Cubillos <cristiancubillos0654@gmail.com>
2024-09-22 01:19:17 -0500

    refactor: Update project structure to implement clean architecture and separate concerns

--- lib/app/UI/pages/home/home_page.dart
+++ lib/app/UI/pages/home/home_page.dart
@@ -1,68 +1,157 @@
+import 'package:best_self/app/UI/pages/home/widgets/mascota_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../controllers/habit_controller.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: appBarHome(Theme.of(context).primaryColor),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              _CustomBackground(size: size),
               Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "Organize your life with Habito",
-                  style: Theme.of(context).textTheme.titleLarge,
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    const PrimaryTitle(),
+                    HabitList(),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              HabitProgressCard(),
-              HabitList(),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+    );
+  }
+
+  AppBar appBarHome(Color color) {
+    return AppBar(
+      backgroundColor: color,
+      leading: const Padding(
+        padding: EdgeInsets.all(5.0),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+              "https://avatars.githubusercontent.com/u/583231?v=4"),
+        ),
+      ),
+      title: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Hello,",
+            style: TextStyle(fontSize: 15),
+          ),
+          Text(
+            "John Doe",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      actions: [
+        IconButton.outlined(
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+        )
+      ],
     );
   }
 }
 
-class HabitProgressCard extends StatelessWidget {
-  final HabitController _controller = Get.find();
+class _CustomBackground extends StatelessWidget {
+  const _CustomBackground({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.blueAccent,
-          borderRadius: BorderRadius.circular(20),
+    final color = Theme.of(context);
+
+    return Stack(
+      children: [
+        SizedBox(
+          height: size.height * 0.28,
+          width: size.width,
+          child: CustomPaint(
+            painter: MultiParablePainter(color.primaryColor),
+          ),
         ),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              "Let's make a habit together",
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Obx(() => CircularProgressIndicator(
-                  value: _controller.completionPercentage.value,
-                  backgroundColor: Colors.white,
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
-                )),
-            SizedBox(height: 10),
-            Obx(() => Text(
-                  "${(_controller.completionPercentage.value * 100).toStringAsFixed(0)}% completed",
-                  style: TextStyle(color: Colors.white),
-                )),
-          ],
+        SizedBox(
+          height: size.height * 0.28,
+          width: size.width,
+          child: Center(child: MascotaScreen()),
         ),
-      ),
+      ],
+    );
+  }
+}
+
+class MultiParablePainter extends CustomPainter {
+  final Color color;
+
+  MultiParablePainter(this.color);
+  @override
+  void paint(Canvas canvas, Size size) {
+    final brush = Paint()..color = color;
+
+    final initialPointY = size.height * 1;
+
+    final controlPointX1 = size.width*0.5;
+    final controlPointY1 = size.width*0.3;
+
+    final path = Path()
+      ..lineTo(0, initialPointY)
+      ..quadraticBezierTo(
+        controlPointX1,
+        controlPointY1,
+        size.width,
+        initialPointY,
+      )
+      ..lineTo(size.width, 0)
+      ..close();
+
+    canvas.drawPath(path, brush);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class PrimaryTitle extends StatelessWidget {
+  const PrimaryTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Monitorea",
+          style: TextStyle(fontSize: 16, color: Colors.grey),
+        ),
+        Text(
+          "tus habitos",
+          style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
@@ -70,7 +159,9 @@ class HabitProgressCard extends StatelessWidget {
 class HabitList extends StatelessWidget {
   final List<String> habits = ["Shopping", "Cycling", "Read a book"];
 
-  HabitController _habitController = Get.find();
+  final HabitController _habitController = Get.find();
+
+  HabitList({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -82,15 +173,42 @@ class HabitList extends StatelessWidget {
         shrinkWrap: true,
         itemCount: habits.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              title: Text(habits[index].title),
-              subtitle: Text("Completed today"),
-              trailing: Icon(Icons.check_circle, color: Colors.green),
-            ),
+          return HabitCard(
+            title: habits[index].title,
+            subtitle: "Esto es una prueba",
+            isCompleted: habits[index].isCompleted,
           );
         },
+      ),
+    );
+  }
+}
+
+class HabitCard extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final bool isCompleted;
+
+  const HabitCard({
+    super.key,
+    required this.title,
+    this.subtitle,
+    required this.isCompleted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context);
+    return Card(
+      color: color.hoverColor,
+      elevation: 0,
+      child: ListTile(
+        leading: const Icon(Icons.air_sharp),
+        title: Text(title),
+        subtitle: Text(subtitle ?? ""),
+        trailing: isCompleted
+            ? const Icon(Icons.check_circle, color: Colors.green)
+            : const Icon(Icons.circle_outlined),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
       ),
       home: HomePage(),
